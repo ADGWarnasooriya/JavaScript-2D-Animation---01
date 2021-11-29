1c96fba4d25962c632d93bae0e385e49d6dc96fb
ADGWarnasooriya <phoenixfire696@gmail.com>
2021-11-29 23:47:17 +0530

    need if condition, because attack images ends up with 9 images

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ var n1 = 0;
 
 function m() {
     n1 = n1 + 1;
-
+    if (n1 == 10) {
+        n1 = 0;
+    }
     var a0 = document.getElementById("a0");
     a0.src = "./png/Attack__00" + n1 + ".png";
 }
