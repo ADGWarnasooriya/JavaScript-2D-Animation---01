d0d90200b8465ca9a9d143fe9f0eed09245c1dcf
ADGWarnasooriya <phoenixfire696@gmail.com>
2021-11-30 23:32:45 +0530

    margin left

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var n1 = 0;
 var p1 = 0;
 
+
 function m() {
     n1 = n1 + 1;
     if (n1 == 10) {
@@ -8,6 +9,7 @@ function m() {
     }
     var a0 = document.getElementById("a0");
     a0.src = "./png/Attack__00" + n1 + ".png";
+    a0.style.marginLeft = "100px";
 }
 
 function n() {
